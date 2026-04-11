a73dd9f21274c94225ac213b40cfdfa7b7ce8727
Hikmat Tamang <hikmattitung1111@gmail.com>
2026-04-11 19:12:47 +0545

    Update

--- about.php
+++ about.php
@@ -26,10 +26,7 @@
   <h2 class="fw-bold h-font text-center">ABOUT US</h2>
   <div class="h-line bg-dark"></div>
   <p class="text-center mt-5">At BhatGaun, every stay is a story waiting to be written. Located at the heart of the village, our hotel offers more than just rooms
-                             it's a serene escape surrounded by brethtaking scenery. Immerse yourself in the perfect harmony of
-                             comfort, luxury, and warm, personalized service, and create memories that will stay with you long after your
-                             journey ends.
-
+                             it's a serene escape surrounded by brethtaking scenery. 
 .</p>
 </div>
 
